765d25519777b451f15a07583af265ef36edadc0
siva.shanthini.e.g <siva.shanthini.e.g@oracle.com>
2025-05-20 11:30:22 +0530

    MaxAbsoluteDifference_1131

--- array/medium/MaxAbsoluteDifference_1131.java
+++ array/medium/MaxAbsoluteDifference_1131.java
@@ -41,18 +41,24 @@ public class MaxAbsoluteDifference_1131 {
         int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE, max4 = Integer.MIN_VALUE;
         int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE, min3 = Integer.MAX_VALUE, min4 = Integer.MAX_VALUE;
         for (int i = 0; i < arr1.length; i++) {
+            // Track the max and min of the expression: arr1[i] + arr2[i] + i
             max1 = Math.max(max1, arr1[i] + arr2[i] + i);
             min1 = Math.min(min1, arr1[i] + arr2[i] + i);
 
+            // Track the max and min of the expression: arr1[i] + arr2[i] - i
             max2 = Math.max(max2, arr1[i] + arr2[i] - i);
             min2 = Math.min(min2, arr1[i] + arr2[i] - i);
 
+            // Track the max and min of the expression: arr1[i] - arr2[i] + i
             max3 = Math.max(max3, arr1[i] - arr2[i] + i);
             min3 = Math.min(min3, arr1[i] - arr2[i] + i);
 
+            // Track the max and min of the expression: arr1[i] - arr2[i] - i
             max4 = Math.max(max4, arr1[i] - arr2[i] - i);
             min4 = Math.min(min4, arr1[i] - arr2[i] - i);
         }
+        // The maximum absolute value expression result will be the largest difference
+        // between max and min for each of the 4 possible expressions
         return Math.max(Math.max(max1 - min1, max2 - min2), Math.max(max3 - min3, max4 - min4));
     }
 
